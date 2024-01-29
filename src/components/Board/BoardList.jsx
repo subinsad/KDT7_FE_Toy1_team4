23ac6b8d70c6256a1bf9e80358251f27961c7f78
subbi <92204014+subinsad@users.noreply.github.com>
2024-01-29 23:11:26 +0900

    feat : dialog onClick추가

--- src/components/Board/BoardList.jsx
+++ src/components/Board/BoardList.jsx
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../Common/Profile";
 import pic1 from "../../assets/profile1.jpg";
 import Button from "../Common/Button";
 
 const BoardList = ({ types }) => {
-  return (
-    <div className={types}>
-      <ul className={"board"}>
-        <li>
-          <a href="">
-            <Profile filename={pic1} />
-            <div className="board__status">출근전</div>
-            <div className="board__title">제목</div>
-            <div className="board__writer">작성자</div>
-          </a>
-          <div className="board__more">
-            <Button className={"btn-more"} />
-            <dialog open>
-              <ul>
+    const [isDialogVisible, setDialogVisible] = useState(false)
+
+    const handleBtn = () => {
+        setDialogVisible(!isDialogVisible)
+    }
+
+    return (
+        <div className={types}>
+            <ul className={"board"}>
                 <li>
-                  <button>수정</button>
+                    <a href="">
+                        <Profile filename={pic1} />
+                        <div className="board__status">출근전</div>
+                        <div className="board__title">제목</div>
+                        <div className="board__writer">작성자</div>
+                    </a>
+                    <div className="board__more">
+                        <Button className={"btn-more"} onClick={handleBtn} />
+                        {isDialogVisible && (
+                            <dialog open>
+                                <ul>
+                                    <li>
+                                        <button>수정</button>
+                                    </li>
+                                    <li>
+                                        <button>삭제</button>
+                                    </li>
+                                </ul>
+                            </dialog>
+                        )}
+
+                    </div>
                 </li>
-                <li>
-                  <button>삭제</button>
-                </li>
-              </ul>
-            </dialog>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+            </ul>
+        </div >
+    );
 };
 
 export default BoardList;
