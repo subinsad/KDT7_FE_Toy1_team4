b35e4074ecf4375b0ee821577ebf0ec2ab878e86
hasungpil <dependspic@gmail.com>
2024-02-02 14:57:19 +0900

    input수정

--- src/components/Form/InputDate.jsx
+++ src/components/Form/InputDate.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Form.scss";
 
-const Date = ({ placehodler, className, ...props }) => {
+const InputDate = ({ placehodler, className, ...props }) => {
   return (
     <input type="date" data-placeholder={placehodler} className={"date " + className} {...props} />
   );
 };
 
-export default Date;
+export default InputDate;
--- src/pages/StyleGuide.jsx
+++ src/pages/StyleGuide.jsx
@@ -19,7 +19,7 @@ import TimeBlock from "../components/TimeBlock";
 import Radio from "../components/Form/Radio";
 import AddFile from "../components/Form/AddFile";
 import ProfileEdit from "../components/Common/ProfileEdit";
-import Date from "../components/Form/Date";
+import InputDate from "../components/Form/InputDate";
 
 const StyleGuide = () => {
   const [modal, setModal] = useState(false);
@@ -118,7 +118,7 @@ const StyleGuide = () => {
       <br />
       <Textarea width={"100%"} height={"10rem"} />
       <br />
-      <Date placehodler={"시작일"} />
+      <InputDate placehodler={"시작일"} />
       <br />
       <br />
       <Checkbox
